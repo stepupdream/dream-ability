60ac8d945e92c0d84b15210e9a544bd4231cc105
stepupdream <stepupdream1002@gmail.com>
2022-09-13 01:25:01 +0900

    [fix] Correction of path errors.

--- src/Database/Console/Migrations/BaseMigrationCommand.php
+++ src/Database/Console/Migrations/BaseMigrationCommand.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace StepUpDream\DreamAbility\Database\Console\Migrations;
 
 use Illuminate\Console\ConfirmableTrait;
-use StepUpDream\DreamAbility\Database\Migrations\Migrator;
+use StepUpDream\DreamAbility\Database\Migrations\Basic\Migrator;
 use StepUpDream\DreamAbilitySupport\Console\BaseCommand;
 use StepUpDream\DreamAbilitySupport\Supports\File\FileOperation;
 
@@ -17,8 +17,7 @@ class BaseMigrationCommand extends BaseCommand
      * Create a new console command instance.
      *
      * @param  \StepUpDream\DreamAbilitySupport\Supports\File\FileOperation  $fileOperation
-     * @param  \StepUpDream\DreamAbility\Database\Migrations\Migrator  $migrator
-     * @return void
+     * @param  \StepUpDream\DreamAbility\Database\Migrations\Basic\Migrator  $migrator
      */
     public function __construct(
         protected FileOperation $fileOperation,
